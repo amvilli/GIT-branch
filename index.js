b15c645376578e7ac26c94a5b45084c2eb88cf3c
villayat <villayat.ali@proton.me>
2024-12-29 21:44:13 +0530

    initial branch commit

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
-
+const path = require("path")
 const app = express();
 // Middlewares .. 
-app.use(express.urlencoded({extended:false}));
-app.get('/', (req,res)=>
-{
-    res.json({msg : "hello world !"})
+app.use(express.urlencoded({ extended: false }));
+
+app.set("view engine ", "ejs")
+app.set("view engine",path.resolve("./view") )
+
+app.get("/", (req, res) => {
+    res.render("home");
+
 })
 app.listen(8000, () => {
     console.log(`Server started on port`);
